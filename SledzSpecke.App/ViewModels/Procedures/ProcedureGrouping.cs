56837210d1a7897855b0e53905641d4dfc71e480
Konrad Niedźwiedzki <githab123@kniedzwiedzki.pl>
2025-03-04 18:30:57 +0100

    update ProceduresListViewModel

---
+++ SledzSpecke.App/ViewModels/Procedures/ProcedureGrouping.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using SledzSpecke.App.Models;
+
+namespace SledzSpecke.App.ViewModels.Procedures
+{
+    // Klasa pomocnicza do grupowania procedur
+    public class ProcedureGrouping : ObservableCollection<Procedure>
+    {
+        public string GroupName { get; private set; }
+
+        public ProcedureGrouping(string groupName, List<Procedure> procedures) : base(procedures)
+        {
+            this.GroupName = groupName;
+        }
+    }
+}
