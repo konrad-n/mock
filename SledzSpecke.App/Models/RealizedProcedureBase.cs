bee7f909ad10abf6216dd6d49e6a16cad7412f8e
Konrad Niedźwiedzki <githab123@kniedzwiedzki.pl>
2025-03-10 17:52:15 +0100

    add proc base

---
+++ SledzSpecke.App/Models/RealizedProcedureBase.cs
@@ -0,0 +1,20 @@
+﻿using SledzSpecke.App.Models.Enums;
+using SQLite;
+
+namespace SledzSpecke.App.Models
+{
+    public abstract class RealizedProcedureBase
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int ProcedureId { get; set; }
+
+        [Indexed]
+        public int SpecializationId { get; set; }
+
+        // Wspólne pola dla obu wersji SMK
+        public DateTime Date { get; set; }
+        public SyncStatus SyncStatus { get; set; }
+        public string AdditionalFields { get; set; } // JSON
+    }
+}
