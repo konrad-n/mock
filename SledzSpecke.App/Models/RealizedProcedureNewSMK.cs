d50c92c32867cefd5ff06ae9fab328bc5e94488a
Konrad Niedźwiedzki <githab123@kniedzwiedzki.pl>
2025-03-10 17:53:06 +0100

    add models

---
+++ SledzSpecke.App/Models/RealizedProcedureNewSMK.cs
@@ -0,0 +1,27 @@
+﻿using SledzSpecke.App.Models.Enums;
+using SQLite;
+
+namespace SledzSpecke.App.Models
+{
+    public class RealizedProcedureNewSMK : RealizedProcedureBase
+    {
+        [Indexed]
+        public int ProcedureRequirementId { get; set; } // Powiązanie z wymaganiem procedury z JSON
+
+        [Indexed]
+        public int? ModuleId { get; set; } // Powiązanie z modułem
+
+        public int CountA { get; set; } // Liczba wykonanych samodzielnie
+        public int CountB { get; set; } // Liczba wykonanych jako asysta
+
+        public DateTime StartDate { get; set; } // Data od
+        public DateTime EndDate { get; set; } // Data do
+
+        // Dodatkowe informacje o procedurze (nie przechowywane w bazie, ale ładowane z JSON)
+        [Ignore]
+        public string ProcedureName { get; set; }
+
+        [Ignore]
+        public string InternshipName { get; set; }
+    }
+}
---
+++ SledzSpecke.App/Models/RealizedProcedureOldSMK.cs
@@ -0,0 +1,35 @@
+﻿using SledzSpecke.App.Models.Enums;
+using SQLite;
+
+namespace SledzSpecke.App.Models
+{
+    public class RealizedProcedureOldSMK : RealizedProcedureBase
+    {
+        public int Year { get; set; } // Rok specjalizacji
+
+        [MaxLength(20)]
+        public string Code { get; set; } // A - operator lub B - asysta
+
+        [MaxLength(100)]
+        public string PerformingPerson { get; set; } // Osoba wykonująca
+
+        [MaxLength(100)]
+        public string Location { get; set; } // Miejsce wykonania
+
+        [Indexed]
+        public int InternshipId { get; set; } // Powiązanie ze stażem
+
+        [MaxLength(100)]
+        public string InternshipName { get; set; } // Nazwa stażu
+
+        [MaxLength(10)]
+        public string PatientInitials { get; set; } // Inicjały pacjenta
+
+        [MaxLength(1)]
+        public string PatientGender { get; set; } // K lub M
+
+        public string AssistantData { get; set; } // Dane osoby wykonującej I i II asystę
+
+        public string ProcedureGroup { get; set; } // Procedura z grupy
+    }
+}
