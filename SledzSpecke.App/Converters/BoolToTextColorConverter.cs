e768b1fb6397abf71e4dfbb6156c11fbe69523ad
Konrad Niedźwiedzki <githab123@kniedzwiedzki.pl>
2025-04-06 20:12:43 +0200

    Change to argb

--- SledzSpecke.App/Converters/BoolToTextColorConverter.cs
+++ SledzSpecke.App/Converters/BoolToTextColorConverter.cs
@@ -8,10 +8,10 @@ namespace SledzSpecke.App.Converters
         {
             if (value is bool isSelected)
             {
-                return isSelected ? Colors.White : Color.FromHex("#082044");
+                return isSelected ? Colors.White : Color.FromArgb("#082044");
             }
 
-            return Color.FromHex("#082044");
+            return Color.FromArgb("#082044");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- SledzSpecke.App/Converters/CodeToColorConverter.cs
+++ SledzSpecke.App/Converters/CodeToColorConverter.cs
@@ -10,13 +10,13 @@ namespace SledzSpecke.App.Converters
             {
                 return code switch
                 {
-                    "A" => Color.FromHex("#24C1DE"),
-                    "B" => Color.FromHex("#F59E0B"),
-                    _ => Color.FromHex("#547E9E"),
+                    "A" => Color.FromArgb("#24C1DE"),
+                    "B" => Color.FromArgb("#F59E0B"),
+                    _ => Color.FromArgb("#547E9E"),
                 };
             }
 
-            return Color.FromHex("#547E9E");
+            return Color.FromArgb("#547E9E");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
