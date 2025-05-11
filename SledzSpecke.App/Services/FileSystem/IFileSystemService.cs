01cccb03264f026de1fa389e1157e1bdce52672a
Konrad Niedźwiedzki <githab123@kniedzwiedzki.pl>
2025-05-11 23:06:31 +0200

    Remove more code

--- SledzSpecke.App/Services/FileSystem/IFileSystemService.cs
+++ SledzSpecke.App/Services/FileSystem/IFileSystemService.cs
@@ -3,21 +3,5 @@
     public interface IFileSystemService
     {
         string AppDataDirectory { get; }
-
-        string GetAppSubdirectory(string subDirectory);
-
-        bool EnsureDirectoryExists(string path);
-
-        Task<string> ReadAllTextAsync(string path);
-
-        Task WriteAllTextAsync(string path, string contents);
-
-        Task<byte[]> ReadAllBytesAsync(string path);
-
-        Task WriteAllBytesAsync(string path, byte[] bytes);
-
-        bool FileExists(string path);
-
-        bool DeleteFile(string path);
     }
 }
